76820b9611ba9e2213e2aca27d83ec856d4b2d9f
irefaat <irefaat@hotmail.com>
2020-11-19 20:06:48 -0500

    first commit

---
+++ FinalTest/FinalTest/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace FinalTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pesron[] person = new Pesron[2] { new Adult("John", "Smith"), new Child("Andrew", "Ibrahim") };
+            foreach(Pesron p in person)
+            {
+                p.PrintName();
+            }
+        }
+    }
+
+    public class Pesron
+    {
+        public string FirstName { get; }
+        public string LasttName { get; }
+
+        public Pesron(string fName , string lNmae)
+        {
+            FirstName = fName;
+            LasttName = lNmae;
+        }
+
+        public void PrintName()
+        {
+            Console.WriteLine(FirstName + " " + LasttName);
+        }
+
+        
+    }
+
+    public class Adult : Pesron
+    {
+        public Adult(string fName , string lName) : base(fName , lName)
+        {
+
+        }
+
+       
+    }
+
+    public class Child : Pesron
+    {
+        public Child(string fName, string lName) : base(fName, lName)
+        {
+
+        }
+
+       
+    }
+}
